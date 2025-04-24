ca855d500dbd376abdb7a4173291b8c1be42af11
Min Han Kyaw <minhankyaw.mh@gmail.com>
2025-04-24 21:01:13 +0700

    password show/hide icon

--- src/modules/admin/user-management/admin/AdminFormView.tsx
+++ src/modules/admin/user-management/admin/AdminFormView.tsx
@@ -1,17 +1,25 @@
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { Link, useLocation, useParams } from 'react-router-dom';
-import { CircleChevronLeft } from 'lucide-react';
-import api from '@/api';
-import { toast } from '@/hooks/use-toast';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Link, useLocation, useParams } from "react-router-dom";
+import { CircleChevronLeft, Eye, EyeOff } from "lucide-react";
+import api from "@/api";
+import { toast } from "@/hooks/use-toast";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from 'react-redux';
-import { hideLoader, openLoader } from '@/store/features/loaderSlice';
-import FormHeader from '@/components/common/FormHeader';
-import { AddAdminPayloadType, UpdateAdminPayloadType } from '@/api/admin/types';
+import { useDispatch } from "react-redux";
+import { hideLoader, openLoader } from "@/store/features/loaderSlice";
+import FormHeader from "@/components/common/FormHeader";
+import { AddAdminPayloadType, UpdateAdminPayloadType } from "@/api/admin/types";
+import { useState } from "react";
 
 const formSchema = z.object({
   first_name: z.string().min(2, {
@@ -28,8 +36,8 @@ const formSchema = z.object({
 });
 
 export default function AdminFormView() {
-
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -41,10 +49,10 @@ export default function AdminFormView() {
   const item: AddAdminPayloadType = id
     ? { ...passedData }
     : {
-      name: "",
-      description: "",
-      createby: 1,
-    };
+        name: "",
+        description: "",
+        createby: 1,
+      };
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -55,54 +63,51 @@ export default function AdminFormView() {
     },
   });
 
-  const { mutate: addAdmin } =
-    api.admin.adminUsers.addAdmin.useMutation({
-      onMutate: () => {
-        dispatch(openLoader());
-      },
-      onSuccess: () => {
-        toast({
-          title: "New Admin added successfully",
-          variant: "success",
-        });
-        navigate("/admin/user-management/admins");
-      },
-      onError: (error) => {
-        form.setError("first_name", { type: "custom", message: error.message });
-        toast({
-          title: error.message,
-          variant: "destructive",
-        });
-      },
-      onSettled: () => {
-        dispatch(hideLoader());
-      },
-    });
+  const { mutate: addAdmin } = api.admin.adminUsers.addAdmin.useMutation({
+    onMutate: () => {
+      dispatch(openLoader());
+    },
+    onSuccess: () => {
+      toast({
+        title: "New Admin added successfully",
+        variant: "success",
+      });
+      navigate("/admin/user-management/admins");
+    },
+    onError: (error) => {
+      form.setError("first_name", { type: "custom", message: error.message });
+      toast({
+        title: error.message,
+        variant: "destructive",
+      });
+    },
+    onSettled: () => {
+      dispatch(hideLoader());
+    },
+  });
 
-  const { mutate: updateAdmin } =
-    api.admin.adminUsers.updateAdmin.useMutation({
-      onMutate: () => {
-        dispatch(openLoader());
-      },
-      onSuccess: () => {
-        toast({
-          title: "Admin updated successfully",
-          variant: "success",
-        });
-        navigate("/admin/admin/user-management/admins");
-      },
-      onError: (error) => {
-        form.setError("first_name", { type: "custom", message: error.message });
-        toast({
-          title: error.message,
-          variant: "destructive",
-        });
-      },
-      onSettled: () => {
-        dispatch(hideLoader());
-      },
-    });
-
+  const { mutate: updateAdmin } = api.admin.adminUsers.updateAdmin.useMutation({
+    onMutate: () => {
+      dispatch(openLoader());
+    },
+    onSuccess: () => {
+      toast({
+        title: "Admin updated successfully",
+        variant: "success",
+      });
+      navigate("/admin/admin/user-management/admins");
+    },
+    onError: (error) => {
+      form.setError("first_name", { type: "custom", message: error.message });
+      toast({
+        title: error.message,
+        variant: "destructive",
+      });
+    },
+    onSettled: () => {
+      dispatch(hideLoader());
+    },
+  });
 
   const onSubmit = async (item: z.infer<typeof formSchema>) => {
     try {
@@ -134,30 +139,33 @@ export default function AdminFormView() {
 
   return (
     <section className="m-4">
-      <FormHeader
-				title="User Management - Admin"
-			/>
+      <FormHeader title="User Management - Admin" />
       <div className="p-6 bg-white rounded-lg">
-        <div className='flex mb-8'>
-          <div className='me-5'>
-            <Link to={'/admin/user-management/admins'}>
-              <CircleChevronLeft className='w-8 h-8 text-secondary hover:text-blue-500' />
+        <div className="flex mb-8">
+          <div className="me-5">
+            <Link to={"/admin/user-management/admins"}>
+              <CircleChevronLeft className="w-8 h-8 text-secondary hover:text-blue-500" />
             </Link>
           </div>
-          <div className='text-base font-semibold mt-1 text-secondary'>
+          <div className="text-base font-semibold mt-1 text-secondary">
             {id ? "Edit Admin" : "Add New Admin"}
           </div>
         </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)}>
-            <div className='grid grid-cols-2 gap-6 mt-5'>
+            <div className="grid grid-cols-2 gap-6 mt-5">
               {/* First Name */}
               <FormField
                 control={form.control}
                 name="first_name"
                 render={({ field }) => (
-                  <FormItem >
-                    <FormLabel>First Name <span className='text-primary font-extrabold text-base'>*</span></FormLabel>
+                  <FormItem>
+                    <FormLabel>
+                      First Name{" "}
+                      <span className="text-primary font-extrabold text-base">
+                        *
+                      </span>
+                    </FormLabel>
                     <FormControl>
                       <Input placeholder="First Name" {...field} />
                     </FormControl>
@@ -170,8 +178,13 @@ export default function AdminFormView() {
                 control={form.control}
                 name="last_name"
                 render={({ field }) => (
-                  <FormItem >
-                    <FormLabel>Last Name <span className='text-primary font-extrabold text-base'>*</span></FormLabel>
+                  <FormItem>
+                    <FormLabel>
+                      Last Name{" "}
+                      <span className="text-primary font-extrabold text-base">
+                        *
+                      </span>
+                    </FormLabel>
                     <FormControl>
                       <Input placeholder="Last Name" {...field} />
                     </FormControl>
@@ -184,8 +197,13 @@ export default function AdminFormView() {
                 control={form.control}
                 name="email"
                 render={({ field }) => (
-                  <FormItem >
-                    <FormLabel>Email <span className='text-primary font-extrabold text-base'>*</span></FormLabel>
+                  <FormItem>
+                    <FormLabel>
+                      Email{" "}
+                      <span className="text-primary font-extrabold text-base">
+                        *
+                      </span>
+                    </FormLabel>
                     <FormControl>
                       <Input placeholder="Email" {...field} />
                     </FormControl>
@@ -193,30 +211,60 @@ export default function AdminFormView() {
                   </FormItem>
                 )}
               />
-              {/* Password */}
               <FormField
                 control={form.control}
                 name="password"
-                render={({ field }) => (
-                  <FormItem >
-                    <FormLabel>Password <span className='text-primary font-extrabold text-base'>*</span></FormLabel>
-                    <FormControl>
-                      <Input type="password" placeholder="Password" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
+                render={({ field }) => {
+                  const togglePasswordVisibility = () => {
+                    setShowPassword(!showPassword);
+                  };
+
+                  return (
+                    <FormItem>
+                      <FormLabel>
+                        Password{" "}
+                        <span className="text-primary font-extrabold text-base">
+                          *
+                        </span>
+                      </FormLabel>
+                      <div className="relative">
+                        <FormControl>
+                          <Input
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <button
+                          type="button"
+                          className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                          onClick={togglePasswordVisibility}
+                          tabIndex={-1}
+                        >
+                          {showPassword ? (
+                            <EyeOff size={16} />
+                          ) : (
+                            <Eye size={16} />
+                          )}
+                        </button>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
               />
-              
             </div>
             <div>
-              <button type="submit" className="bg-secondary rounded-sm p-2 px-6 text-white mt-7">
+              <button
+                type="submit"
+                className="bg-secondary rounded-sm p-2 px-6 text-white mt-7"
+              >
                 {id ? "Update" : "Save"}
               </button>
             </div>
           </form>
         </Form>
       </div>
-    </section >
-  )
+    </section>
+  );
 }
